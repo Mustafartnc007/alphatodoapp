ce1cc3e40c0cb7cbb8c67f93b50cb9684c8819fc
Mustafa Ertunç <mustafartnc007@gmail.com>
2025-07-10 16:06:38 +0300

    Initial Commit

---
+++ alphatodoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alphatodoapp
+//
+//  Created by Mustafa Ertunç on 10.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alphatodoapp/alphatodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  alphatodoappApp.swift
+//  alphatodoapp
+//
+//  Created by Mustafa Ertunç on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct alphatodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
